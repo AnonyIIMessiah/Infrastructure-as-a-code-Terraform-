dd1978798635d529ba3b538975c298a7a055d393
AnonyIIMessiah <vaman1650.av@gmail.com>
2023-12-12 11:51:09 +0530

    16

---
+++ 16-AWS-CloudWatch/terraform_manifest/c12-route53-dnsregistration.tf
@@ -0,0 +1,36 @@
+# DNS Registration
+# Default DNS
+resource "aws_route53_record" "default_dns" {
+  zone_id = data.aws_route53_zone.myDomain.zone_id
+  name    = "cloudwatch.devopscamp.shop"
+  type    = "A"
+  alias {
+    name                   = module.alb.lb_dns_name
+    zone_id                = module.alb.lb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+# # App1 DNS
+# resource "aws_route53_record" "app1_dns" {
+#   zone_id = data.aws_route53_zone.myDomain.zone_id
+#   name    = var.app1_dns_name
+#   type    = "A"
+#   alias {
+#     name                   = module.alb.lb_dns_name
+#     zone_id                = module.alb.lb_zone_id
+#     evaluate_target_health = true
+#   }
+# }
+
+# # App2 DNS
+# resource "aws_route53_record" "app2_dns" {
+#   zone_id = data.aws_route53_zone.myDomain.zone_id
+#   name    = var.app2_dns_name
+#   type    = "A"
+#   alias {
+#     name                   = module.alb.lb_dns_name
+#     zone_id                = module.alb.lb_zone_id
+#     evaluate_target_health = true
+#   }
+# }
